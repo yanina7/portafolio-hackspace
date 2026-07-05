55ca2f224bc1869b1438ca226f4c14e3cc9ff472
yanina.mercado.contractor <yanina.mercado.contractor@bbva.com>
2026-07-05 12:44:59 -0500

    update

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+const typeds = new Typed('.typed-2', {
+    strings: [
+        '<i class="nombre">Cloud Data Engineer</i>',
+        '<i class="nombre">Business Data Analytics</i>',
+    ],
+	typeSpeed: 75,
+	startDelay: 300,
+	backSpeed: 75,
+	smartBackspace: true,
+	shuffle: false,
+	backDelay: 1500,
+	loop: true,
+	loopCount: false,
+	contentType: 'html', 
+});
